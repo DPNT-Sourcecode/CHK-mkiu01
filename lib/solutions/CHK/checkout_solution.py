dbb09700af19d239caf0413da4cff1e413829e24
sbx_user1051 <sbx_user1051@169.254.130.157>
2023-06-10 18:54:08 +0000

    Sat Jun 10 18:54:08 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,14 +6,22 @@ special_offers = {'A': (3, 130), 'B': (2, 45)}
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    total = 0
+    # dict mapping item to count in the basket
     item_counts = {}
     for item in skus:
+        if item in item_counts:
+            item_counts[item] += 1
+        else:
+            item_counts[item] = 1
+    # sum up the total
+    total = 0
+    for item, count in item_counts.items():
         if item in sku_prices.keys():
-            total += sku_prices[item]
+            total += sku_prices[item] * count
         else:
             return -1
     return total
+
 
 
 
