cabec82f81c0c75b9ea0a8855e984bdf4dbaabe0
sbx_user1051 <sbx_user1051@169.254.130.157>
2023-06-10 20:37:10 +0000

    Sat Jun 10 20:37:10 UTC 2023

--- lib/solutions/CHK/parse_prices.py
+++ lib/solutions/CHK/parse_prices.py
@@ -5,11 +5,19 @@ with open("prices.txt") as file:
     while line := file.readline():
         items.append([x.strip() for x in line.rstrip().split("|") if x])
 
-print(items)
 print("sku_prices = {")
 for item in items:
     print(f"    '{item[0]}': {item[1]},")
 print("}")
+
+print("special_offers_discount = {")
+for item in items:
+    special_offers = item[2]
+    if len(special_offers)>0:
+        for special_offer in special_offers.split(", "):
+            print(special_offer)
+#    print(f"    '{item[0]}': {item[1]},")
+#print("}")
 
 
 
