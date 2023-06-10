d9f739808e9823612e851a5baeb018afb13cdea5
sbx_user1051 <sbx_user1051@169.254.130.157>
2023-06-10 20:16:08 +0000

    Sat Jun 10 20:16:08 UTC 2023

--- test/solution_tests/CHK/test_checkout_r3.py
+++ test/solution_tests/CHK/test_checkout_r3.py
@@ -10,3 +10,13 @@ class TestCheckoutR3():
     def test_checkout_3f(self):
         assert checkout("FFF") == 20
 
+    def test_checkout_4f(self):
+        assert checkout("FFFF") == 30
+
+    def test_checkout_5f(self):
+        assert checkout("FFFFF") == 40
+
+    def test_checkout_6f(self):
+        assert checkout("FFFFFF") == 40
+
+
