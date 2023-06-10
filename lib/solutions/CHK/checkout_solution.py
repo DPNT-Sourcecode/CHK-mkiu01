7e20eac51c934831f60be7d91718285e3a9b908b
sbx_user1051 <sbx_user1051@169.254.130.157>
2023-06-10 18:51:08 +0000

    Sat Jun 10 18:51:08 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,16 +1,19 @@
-
-
 sku_prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+
+# Special offers dict mapping item to a tuple (count, price)
+special_offers = {'A': (3, 130), 'B': (2, 45)}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     total = 0
+    item_counts = {}
     for item in skus:
         if item in sku_prices.keys():
             total += sku_prices[item]
         else:
             return -1
     return total
+
 
 
