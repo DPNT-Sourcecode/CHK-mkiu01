a94ff4ea5e717a9e4322e7ba1c53fe86fac9739e
sbx_user1051 <sbx_user1051@169.254.130.157>
2023-06-10 19:09:38 +0000

    Sat Jun 10 19:09:38 UTC 2023

--- test/solution_tests/CHK/test_checkout_r2.py
+++ test/solution_tests/CHK/test_checkout_r2.py
@@ -3,3 +3,7 @@ from solutions.CHK.checkout_solution import checkout
 class TestCheckoutR2():
     def test_checkout_e(self):
         assert checkout("E") == 40
+
+    def test_checkout_2e_b(self):
+        assert checkout("EEB") == 80
+
