603fe786fd549745f42ccbb644ea3eb716971563
sbx_user1051 <sbx_user1051@169.254.130.157>
2023-06-10 20:46:10 +0000

    Sat Jun 10 20:46:10 UTC 2023

--- lib/solutions/CHK/parse_prices.py
+++ lib/solutions/CHK/parse_prices.py
@@ -11,13 +11,18 @@ for item in items:
     print(f"    '{item[0]}': {item[1]},")
 print("}")
 
-print("special_offers_discount = {")
+offer_discount_list = []
 for item in items:
     special_offers = item[2]
     if len(special_offers)>0:
         for special_offer in special_offers.split(", "):
             print(special_offer)
-            if re.match(, special_offer):
+            parse_offer = re.match("([1-9])([A-Z]) for (\d+)", special_offer)
+            if parse_offer:
+                offer_discount_list.append((parse_offer.group(1), parse_offer.group(2), parse_offer.group(3)))
+
+print(offer_discount_list)
+print("special_offers_discount = {")
 #    print(f"    '{item[0]}': {item[1]},")
 #print("}")
 
